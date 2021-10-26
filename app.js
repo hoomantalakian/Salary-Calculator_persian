e2c912d8402315d264bd8dc6ef9f185759ecafe0
Hooman Talakian <hooman.talakian@gmail.com>
2021-10-26 05:52:26 +0330

    first edits

--- app.js
+++ app.js
@@ -4,34 +4,40 @@
 // const overWorkHourlyWage = 120000;
 // const workedHours = 41;
 // ====== DOM ======
+
 const standradHours = document.querySelector("#standradHours")
 const hourlyWage = document.querySelector("#hourlyWage")
 const overWorkHourlyWage = document.querySelector("#overWorkHourlyWage")
 const workedHours = document.querySelector("#workedHours")
+
 const calculateButton = document.querySelector("#claculateButton")
 const claculateReset = document.querySelector("#claculateReset")
+const ul = document.querySelector("#listContainer")
+
 
 //================
-const overWorkedHours = Number(workedHours.value) - Number(standradHours.value);
-const standardTotalWage = Number(workedHours.value) * Number(hourlyWage.value);
-const overWorkedTotalWage = overWorkedHours * Number(overWorkHourlyWage.value);
 
-if (workedHours <= standradHours) {
-    var total = standardTotalWage;
-}
-else var total = standardTotalWage + overWorkedTotalWage;
 
 // console.log(total);
 // ==============================
 
-calculateButton.addEventListener("click", (e) => {
-    const li = document.createElement("li")
-    const ul = document.createElement("ul")
-    li.textContent = total
-    ul.appendChild(li)
+calculateButton.addEventListener("click", () => {
+    if (standradHours.value && hourlyWage.value && overWorkHourlyWage.value && workedHours.value) {
+        const overWorkedHours = Number(workedHours.value) - Number(standradHours.value);
+        const standardTotalWage = Number(workedHours.value) * Number(hourlyWage.value);
+        const overWorkedTotalWage = overWorkedHours * Number(overWorkHourlyWage.value);
 
-})
-console.log(total)
+        if (workedHours <= standradHours) {
+            var total = standardTotalWage;
+        } else var total = standardTotalWage + overWorkedTotalWage;
+        //==================
+        const li = document.createElement("li")
+        li.textContent = `جمع حقوق دریافتی شما: ${total} تومان میباشد.`
+        ul.appendChild(li)
+        calculateButton.disabled = true;
+    } else { alert("لطفا تمام فیلد ها را پر کنید !") }
+});
+// console.log(total)
 
 
 
